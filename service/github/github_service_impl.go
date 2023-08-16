93cad882997853704f80361db185e868a3d7f508
tae2089 <tae2089002@gmail.com>
2023-08-16 17:42:53 +0900

    refactor: :recycle: add error handling and GethooksForRepo comment (#4)

--- service/github/github_service_impl.go
+++ service/github/github_service_impl.go
@@ -15,7 +15,7 @@ type gitServiceImpl struct {
 
 var _ GitService = (*gitServiceImpl)(nil)
 
-// GetHooksForRepo
+// GetHooksForRepo functions to output hooks stored in the repo.
 func (g gitServiceImpl) GetHooksForRepo(ctx context.Context, hookDto domain.RequestGithubWebhookDto) ([]*github.Hook, error) {
 	//TODO implement me
 	hooks, _, err := g.client.Repositories.ListHooks(ctx, hookDto.Owner, hookDto.Repo, &github.ListOptions{})
@@ -28,7 +28,7 @@ func (g gitServiceImpl) GetHooksForRepo(ctx context.Context, hookDto domain.Requ
 // RegisterWebhookForJenkins is a function that registers a webhook in a GitHub repository for  Jenkins. Since we're using HTTPS, insecure_ssl is set to false.
 // Also, if at least one webhook is already registered, registration will fail.
 func (g gitServiceImpl) RegisterWebhookForJenkins(ctx context.Context, hookDto domain.RequestGithubWebhookDto) error {
-	err, _ := g.isExistWebhook(ctx, hookDto.Owner, hookDto.Repo)
+	_, err := g.isExistWebhook(ctx, hookDto.Owner, hookDto.Repo)
 	if err != nil {
 		return err
 	}
@@ -53,8 +53,11 @@ func (g gitServiceImpl) RegisterWebhookForJenkins(ctx context.Context, hookDto d
 
 // ModifyWebhookForJenkins is used to change the URL of a webhook. The data required in the request is the same as RegisterWebhookForJenkins.
 func (g gitServiceImpl) ModifyWebhookForJenkins(ctx context.Context, hookDto domain.RequestGithubWebhookDto) error {
-	_, id := g.isExistWebhook(ctx, hookDto.Owner, hookDto.Repo)
+	id, err := g.isExistWebhook(ctx, hookDto.Owner, hookDto.Repo)
 	if id == nil {
+		if err != nil {
+			return err
+		}
 		return exception.NotFoundHooks
 	}
 	hook, _, err := g.client.Repositories.GetHook(ctx, hookDto.Owner, hookDto.Repo, *id)
@@ -72,13 +75,13 @@ func (g gitServiceImpl) ModifyWebhookForJenkins(ctx context.Context, hookDto dom
 // isExistWebhook checks to see if the hook exists in the repository, and if so, returns an error and the hook's id.
 // If not, it returns nil. If you're updating a hook, you'll need the id value and use it when returning.
 // If you're registering a hook, you just need to check for an error.
-func (g gitServiceImpl) isExistWebhook(ctx context.Context, owner, repoName string) (error, *int64) {
+func (g gitServiceImpl) isExistWebhook(ctx context.Context, owner, repoName string) (*int64, error) {
 	hooks, _, err := g.client.Repositories.ListHooks(ctx, owner, repoName, nil)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	if len(hooks) != 0 {
-		return exception.AlreadyHookExistsError, hooks[0].ID
+		return hooks[0].ID, exception.AlreadyHookExistsError
 	}
 	// webhook not exist
 	return nil, nil
