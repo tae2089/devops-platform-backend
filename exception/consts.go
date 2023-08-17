7b3abed8d3e9209af62976e083619c6c9366846d
tae2089 <tae2089002@gmail.com>
2023-08-17 19:08:50 +0900

    :seedling: feature: add NotFoundFile error (#5)

--- exception/consts.go
+++ exception/consts.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	AlreadyHookExistsError = errors.New("Hook is already exists ")
 	NotFoundHooks          = errors.New("Not Found Hooks")
+	NotFoundFile           = errors.New("Not Found File")
 )
