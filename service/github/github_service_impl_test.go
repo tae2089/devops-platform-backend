1cb3c059344108d902e65f89c513d8005b38fdc7
tae2089 <tae2089002@gmail.com>
2023-08-17 19:26:37 +0900

    test: :test_tube: update file adn create file (#5)

---
+++ service/github/github_service_impl_test.go
@@ -0,0 +1,43 @@
+package github_test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tae2089/devops-platform-backend/config"
+	"github.com/tae2089/devops-platform-backend/domain"
+	"github.com/tae2089/devops-platform-backend/service/github"
+)
+
+func Test_gitServiceImpl_UploadFile(t *testing.T) {
+	gitConfig := config.GetGithubConfig()
+	g := github.NewGitService(gitConfig)
+	t.Run("upload file", func(t *testing.T) {
+		t.Skip("")
+		hookDto := domain.RequestUploadFileDto{
+			Owner:   "<GITUHB USER ID>",
+			Repo:    "<GITUH REPON NAME>",
+			Path:    "<FILE PATH>",
+			Branch:  "<BRANCH>",
+			Content: []byte("<CONTENT>"),
+		}
+		err := g.UploadFile(context.Background(), hookDto)
+		assert.Nil(t, err)
+	})
+
+	t.Run("not found file", func(t *testing.T) {
+		t.Skip("")
+		hookDto := domain.RequestUploadFileDto{
+			Owner:   "<GITUHB USER ID>",
+			Repo:    "<GITUH REPON NAME>",
+			Path:    "<FILE PATH>",
+			Branch:  "<BRANCH>",
+			Content: []byte("<CONTENT>"),
+		}
+		err := g.UploadFile(context.Background(), hookDto)
+		log.Println(err)
+		assert.Nil(t, err)
+	})
+}
