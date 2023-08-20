bee5bb0de768bf87e3a0060344acda05ddc0a1d5
tae2089 <tae2089002@gmail.com>
2023-08-20 20:40:46 +0900

    :seedling: feature: add jenkins usecase (#26)

---
+++ usecase/jenkins_usecase.go
@@ -0,0 +1,21 @@
+package usecase
+
+import (
+	"net/http"
+
+	"github.com/tae2089/devops-platform-backend/util/github"
+	"github.com/tae2089/devops-platform-backend/util/jenkins"
+	"github.com/tae2089/devops-platform-backend/util/slack"
+)
+
+type JenkinsUsecase interface {
+	RegisterLunchPayment(request *http.Request) error
+}
+
+func NewJenkinsUsecase(slackUtil slack.SlackUtil, jenkinsUtil jenkins.JenkinsUtil, githubUtil github.GithubUtil) JenkinsUsecase {
+	return &jenkinsUsecaseImpl{
+		slackUtil,
+		jenkinsUtil,
+		githubUtil,
+	}
+}
