1562275f3fb3a89047a3de5ab18830d8f9253454
tae2089 <tae2089002@gmail.com>
2023-08-22 15:12:41 +0900

    :seedling: feature: implement jenkins file templates (#28)

--- util/jenkins/jenkins_util_impl.go
+++ util/jenkins/jenkins_util_impl.go
@@ -2,19 +2,50 @@ package jenkins
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/bndr/gojenkins"
+	"github.com/tae2089/devops-platform-backend/domain"
 )
+
+var _ Util = (*utilImpl)(nil)
 
 type utilImpl struct {
 	jenkins *gojenkins.Jenkins
 }
 
-var _ Util = (*utilImpl)(nil)
+// GetJenkinsJobContent implements Util.
+func (j *utilImpl) GetJenkinsJobContent(repository, branch, project, webhook, jenkinsFile string) string {
+
+	url := fmt.Sprintf("git@github.com:%s.git", repository)
+	jenkinsJob := domain.JenkinsJob{
+		GitURL:       url,
+		BranchName:   branch,
+		TeamName:     project,
+		FileName:     jenkinsFile,
+		WebhookToken: webhook,
+	}
+	return jenkinsJob.Write()
+}
+
+// GetFrontJobContent implements Util.
+func (j *utilImpl) GetJenkinsFrontFileContent(repository string, branch string, slackChannel string, buckName string, CloudFrontID string, project string) string {
+	jenkinsJob := &domain.JenkinsFrontFile{
+		GitURL:       fmt.Sprintf("git@github.com:%s.git", repository),
+		Branch:       branch,
+		SlackChannel: slackChannel,
+		BuckName:     buckName,
+		CloudFrontID: CloudFrontID,
+		AwsProfile:   project,
+	}
+	return jenkinsJob.Write()
+}
 
 func (j *utilImpl) CreateJob(jobName, folderName, content *string) (*gojenkins.Job, error) {
 	_, err := j.jenkins.CreateJobInFolder(context.Background(), *content, *jobName, *folderName)
 	if err != nil {
+		log.Println(err)
 		return nil, err
 	}
 	return nil, nil
