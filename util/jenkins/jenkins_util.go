d041d89866a94fe7f16ba0e4f7940e2203448375
tae2089 <tae2089002@gmail.com>
2023-08-22 15:12:41 +0900

    :seedling: feature: implement jenkins file templates (#28)

--- util/jenkins/jenkins_util.go
+++ util/jenkins/jenkins_util.go
@@ -12,13 +12,14 @@ type Util interface {
 	CreateJob(jobName, folderName, content *string) (*gojenkins.Job, error)
 	UpdateJob(ctx context.Context, jobName, folderName, content *string) error
 	DeleteJob(ctx context.Context, jobName, folderName *string) error
+	GetJenkinsFrontFileContent(gitURL, branch, slackChannel, buckName, CloudFrontID, project string) string
+	GetJenkinsJobContent(repository, branch, project, webhook, jenkinsFile string) string
 }
 
 func NewJenkinsUtil(jenkinsConfig *config.Jenkins) Util {
 	jenkinsClient := gojenkins.CreateJenkins(nil, jenkinsConfig.URL, jenkinsConfig.User, jenkinsConfig.PassWord)
 	_, err := jenkinsClient.Init(context.Background())
 	if err != nil {
-		//TODO: change to handle
 		log.Println(err)
 		return nil
 	}
