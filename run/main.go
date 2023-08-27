be63049ad708237b269d952f13d238195897df38
tae2089 <tae2089002@gmail.com>
2023-08-27 19:45:47 +0900

    :seedling: feature: add line about declaring db client (#22)

--- run/main.go
+++ run/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	router "github.com/tae2089/devops-platform-backend/api/router"
-	_ "github.com/tae2089/devops-platform-backend/config"
+	config "github.com/tae2089/devops-platform-backend/config"
+	"github.com/tae2089/devops-platform-backend/ent"
 )
 
 func main() {
+	client, err := ent.Open("postgres", config.GetDsn())
+	if err != nil {
+		log.Fatalf("failed opening connection to mysql: %v", err)
+	}
+	defer client.Close()
 	timeout := time.Duration(3) * time.Second
 	gin := gin.Default()
 	router.SetUp(timeout, gin)
