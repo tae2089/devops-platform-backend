952ad693619f9aeb462095db80b06497260a2645
tae2089 <tae2089002@gmail.com>
2023-08-29 22:41:10 +0900

    :seedling: feature: implement create project feature (#38)

--- usecase/jenkins_usecase_impl.go
+++ usecase/jenkins_usecase_impl.go
@@ -1,12 +1,15 @@
 package usecase
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
 
 	goSlack "github.com/slack-go/slack"
 	"github.com/tae2089/devops-platform-backend/domain"
+	"github.com/tae2089/devops-platform-backend/exception"
+	"github.com/tae2089/devops-platform-backend/repository"
 	"github.com/tae2089/devops-platform-backend/util/github"
 	"github.com/tae2089/devops-platform-backend/util/jenkins"
 	"github.com/tae2089/devops-platform-backend/util/slack"
@@ -15,9 +18,35 @@ import (
 var _ (JenkinsUsecase) = (*jenkinsUsecaseImpl)(nil)
 
 type jenkinsUsecaseImpl struct {
-	slackUtil   slack.Util
-	jenkinsUtil jenkins.Util
-	githubUtil  github.Util
+	slackUtil      slack.Util
+	jenkinsUtil    jenkins.Util
+	githubUtil     github.Util
+	userRepository repository.UserRepository
+}
+
+// RegisteProject implements JenkinsUsecase.
+func (j *jenkinsUsecaseImpl) RegistProject(request *http.Request) error {
+	slashCommand, err := j.slackUtil.GetSlashCommandParse(request)
+	if err != nil {
+		return err
+	}
+	user, err := j.userRepository.FindBySlackID(context.Background(), slashCommand.UserID)
+
+	if err != nil {
+		return exception.IsEntityNotFound(err)
+	}
+
+	if err = exception.IsAdminRole(user.Roles); err != nil {
+		return err
+	}
+
+	var modalRequest goSlack.ModalViewRequest = goSlack.ModalViewRequest{}
+	err = j.slackUtil.OpenView(slashCommand.TriggerID, modalRequest)
+	if err != nil {
+		fmt.Printf("Error opening view: %s", err)
+		return err
+	}
+	return nil
 }
 
 // RegistFrontJob implements JenkinsUsecase.
