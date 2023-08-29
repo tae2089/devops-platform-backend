51cb30c7d1b0c311b40d902506a749ad92bf5ecd
tae2089 <tae2089002@gmail.com>
2023-08-29 22:40:39 +0900

    :seedling: feature: create excetpion checking (#38)

--- exception/consts.go
+++ exception/consts.go
@@ -6,4 +6,6 @@ var (
 	AlreadyHookExistsError = errors.New("Hook is already exists ")
 	NotFoundHooks          = errors.New("Not Found Hooks")
 	NotFoundFile           = errors.New("Not Found File")
+	NotFoundEntity         = errors.New("Not Found Entity")
+	Forbidden              = errors.New("Forbidden")
 )
