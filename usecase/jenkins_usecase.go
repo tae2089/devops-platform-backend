052f2362b2b4623fe9a862e62ac34590c7b261a4
tae2089 <tae2089002@gmail.com>
2023-08-29 22:41:10 +0900

    :seedling: feature: implement create project feature (#38)

--- usecase/jenkins_usecase.go
+++ usecase/jenkins_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"net/http"
 
+	"github.com/tae2089/devops-platform-backend/repository"
 	"github.com/tae2089/devops-platform-backend/util/github"
 	"github.com/tae2089/devops-platform-backend/util/jenkins"
 	"github.com/tae2089/devops-platform-backend/util/slack"
@@ -11,12 +12,14 @@ import (
 type JenkinsUsecase interface {
 	RegisterLunchPayment(request *http.Request) error
 	RegistJob(request *http.Request) error
+	RegistProject(request *http.Request) error
 }
 
-func NewJenkinsUsecase(slackUtil slack.Util, jenkinsUtil jenkins.Util, githubUtil github.Util) JenkinsUsecase {
+func NewJenkinsUsecase(slackUtil slack.Util, jenkinsUtil jenkins.Util, githubUtil github.Util, userRepository repository.UserRepository) JenkinsUsecase {
 	return &jenkinsUsecaseImpl{
 		slackUtil,
 		jenkinsUtil,
 		githubUtil,
+		userRepository,
 	}
 }
