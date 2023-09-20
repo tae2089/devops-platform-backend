19c774ead463babda634f9bf18dbe6a1ce6959e4
tae2089 <tae2089002@gmail.com>
2023-09-20 20:58:39 +0900

    update get dockerfile usecase

--- usecase/docker_usecase.go
+++ usecase/docker_usecase.go
@@ -1,14 +1,12 @@
 package usecase
 
 import (
-	"net/http"
-
 	"github.com/tae2089/devops-platform-backend/util/docker"
 	"github.com/tae2089/devops-platform-backend/util/slack"
 )
 
 type DockerUsecase interface {
-	GetDockerFile(request *http.Request) error
+	GetDockerFile(imageName string) string
 }
 
 func NewDockerUsecase(slackUtil slack.Util, dockerUtil docker.Util) DockerUsecase {
--- usecase/docker_usecase_impl.go
+++ usecase/docker_usecase_impl.go
@@ -1,44 +1,20 @@
 package usecase
 
 import (
-	"net/http"
-
 	"github.com/tae2089/devops-platform-backend/util/docker"
 	"github.com/tae2089/devops-platform-backend/util/slack"
+	"strings"
 )
+
+var _ (DockerUsecase) = (*dockerUsecase)(nil)
 
 type dockerUsecase struct {
 	slackUtil  slack.Util
 	dockerUtil docker.Util
 }
 
-// GetDockerFile implements DockerUsecase.
-func (d *dockerUsecase) GetDockerFile(request *http.Request) error {
-	slackCommand, err := d.slackUtil.GetSlashCommandParse(request)
-	if err != nil {
-		return err
-	}
-	templateData := ""
-	switch slackCommand.Text {
-	case "java":
-		templateData = d.dockerUtil.GetJava()
-		break
-	case "go":
-		templateData = d.dockerUtil.GetGolang()
-		break
-	case "js":
-		templateData = d.dockerUtil.GetJavaScript()
-		break
-	default:
-		templateData = "NONE"
-		break
-	}
-	blocks := d.slackUtil.GetDockerCodeBlocks(templateData)
-	var channelId string = slackCommand.ChannelID
-	if slackCommand.ChannelName == "directmessage" {
-		channelId = slackCommand.UserID
-	}
-	return d.slackUtil.PostMessageWithBlocks(channelId, blocks)
+func (d dockerUsecase) GetDockerFile(lang string) string {
+	lang = strings.ToLower(lang)
+	file := d.dockerUtil.GetFileByLang(lang)
+	return file
 }
-
-var _ (DockerUsecase) = (*dockerUsecase)(nil)
