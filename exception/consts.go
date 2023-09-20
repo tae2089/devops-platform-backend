28d1b8fc93288b3bd602c4dcdacb27c1fbc22323
tae2089 <tae2089002@gmail.com>
2023-09-20 19:47:42 +0900

    :seedling: feature: add exception for unauthorized

--- exception/consts.go
+++ exception/consts.go
@@ -8,4 +8,5 @@ var (
 	NotFoundFile           = errors.New("Not Found File")
 	NotFoundEntity         = errors.New("Not Found Entity")
 	Forbidden              = errors.New("Forbidden")
+	NotAuthorized          = errors.New("Not authorized")
 )
